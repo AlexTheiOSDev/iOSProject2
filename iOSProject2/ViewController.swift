e79d477ac1332b31ce374b8be162015f8106919b
AlexTheiOSDev <akovtunenko@sphereinc.com>
2016-12-15 17:58:31 +0200

    adfs

--- iOSProject2/ViewController.swift
+++ iOSProject2/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import iosProjectCommon
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
